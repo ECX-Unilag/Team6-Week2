fd0a4449e753d3513eab15a19186d131955ab402
alameen <alameenraji31@gmail.com>
2020-08-16 12:41:44 +0100

    created article models

--- mag_ng/articles/admin.py
+++ mag_ng/articles/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import ArticleModel, ImageModel, TextModel
 
-# Register your models here.
+
+class ArticleImageAdmin(admin.StackedInline):
+    model = ImageModel
+    extra = 1
+
+
+class ArticleTextAdmin(admin.StackedInline):
+    model = TextModel
+    extra = 1
+
+
+@admin.register(ArticleModel)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleImageAdmin, ArticleTextAdmin]
+
+    class Meta:
+        model = ArticleModel
+
+
+admin.register(ImageModel,ArticleImageAdmin)
+
+
+admin.register(TextModel,ArticleTextAdmin)
